53c7e513bec2f78e2eb76427e5b9d4f5b00379c3
Tanzeem Alam <tanzeemalam97@gmail.com>
2026-07-06 13:28:45 +0530

    register and verifyOTP pages added

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,35 +1,66 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import settingIcon from "../assets/setting.png";
 import userIcon from "../assets/user.png";
 import { useSelector } from "react-redux";
 
 const Header = () => {
   const dispatch = useDispatch();
-  const {user} = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth);
 
   const [currentTime, setCurrentTime] = useState("");
   const [currentDate, setCurrentDate] = useState("");
 
-  useEffect(()=>{
-   const updateDateTime = () =>{
-    const now = new Date();
+  useEffect(() => {
+    const updateDateTime = () => {
+      const now = new Date();
 
-    const hours = now.getHours() % 12 || 12;
-    const minutes = now.getMinutes().toString().padStart(2, "0");
-    const ampm = now.getHours() >= 12 ? "PM" : "AM";
-    setCurrentTime(`${hours}:${minutes} ${ampm}`);
-  
-    const options = { month : "short", day: "numeric", year: "numeric"};
-    setCurrentDate(now.toLocaleDateString("en-IND", options));
-   };
+      const hours = now.getHours() % 12 || 12;
+      const minutes = now.getMinutes().toString().padStart(2, "0");
+      const ampm = now.getHours() >= 12 ? "PM" : "AM";
+      setCurrentTime(`${hours}:${minutes} ${ampm}`);
 
-   updateDateTime();
+      const options = { month: "short", day: "numeric", year: "numeric" };
+      setCurrentDate(now.toLocaleDateString("en-IND", options));
+    };
 
-   const intervalId = setInterval(updateDateTime, 1000);
+    updateDateTime();
 
-   return () => clearInterval(intervalId);
-  }, [])
-  return <></>;
+    const intervalId = setInterval(updateDateTime, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      <header className="absolute top-0 bg-white w-full py-4 px-6 left-0 shadow-md flex justify-between items-center">
+        {/* LEFT SIDE */}
+        <div className="flex items-center gap-2">
+          <img src={userIcon} alt="userIcon" className="w-8 h-8" />
+          <div className="flex flex-col">
+            <span className="text-sm font-medium sm:text-lg lg:text-xl sm:font-semibold ">
+              {user && user.name}
+            </span>
+            <span className="text-sm font-medium sm:text-lg sm:font-medium ">
+              {user && user.role}
+            </span>
+          </div>
+        </div>
+        {/* RIGHT SIDE */}
+        <div className="hidden md:flex items-center gap-2">
+          <div className="flex flex-col text-sm lg:text-base items-end font-semibold "></div>
+          <span>{currentTime}</span>
+          <span>{currentDate}</span>
+        </div>
+        <span className="bg-black h-14 w-[2px] " />
+        <img
+          src={settingIcon}
+          alt="settingIcon"
+          className="w-8 h-8"
+          onClick={() => toggleSettingPopup()}
+        />
+      </header>
+    </>
+  );
 };
 
 export default Header;
--- client/src/pages/OTP.jsx
+++ client/src/pages/OTP.jsx
@@ -1,9 +1,129 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/black-logo.png";
 import logo_with_title from "../assets/logo-with-title.png";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { otpVerification, resetAuthSlice } from "../store/slices/authSlice";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const OTP = () => {
-  return <></>;
+  const { email } = useParams();
+
+  const [otp, setOtp] = useState("");
+
+  const dispatch = useDispatch();
+  const navigateTo = useNavigate();
+
+  const { loading, error, message, user, isAuthenticated } = useSelector(
+    (state) => state.auth,
+  );
+
+  const handleOtpVerification = (e) => {
+    e.preventDefault();
+
+    dispatch(otpVerification(email, otp));
+  };
+
+  useEffect(() => {
+    if (message) {
+      toast.success(message);
+
+      dispatch(resetAuthSlice());
+
+      navigateTo("/");
+    }
+
+    if (error) {
+      toast.error(error);
+
+      dispatch(resetAuthSlice());
+    }
+  }, [dispatch, error, isAuthenticated, loading]);
+
+  // if (isAuthenticated) {
+
+  //   return <Navigate to={"/"} />;
+
+  // }
+
+  return (
+    <>
+      <div className="flex flex-col justify-center md:flex-row h-screen">
+        {/* LEFT SIDE */}
+
+        <div className="w-full md:w-1/2 flex items-center justify-center bg-white p-8 relative ">
+          <Link
+            to={"/register"}
+            className="border-2 border-black rounded-3xl font-bold w-52 py-2 px-4 fixed top-10 -left-28 hover:bg-black hover:text-white transition duration-300 text-end"
+          >
+            Back
+          </Link>
+
+          <div className="max-w-sm w-full">
+            <div className="flex justify-center mb-12">
+              <div className="rounded-full flex items-center justify-center ">
+                <img src={logo} alt="logo" className="h-auto w-24" />
+              </div>
+            </div>
+
+            <h1 className="text-4xl font-medium text-center mb-12 overflow-hidden">
+              Check your mailbox
+            </h1>
+
+            <p className="text-gray-800 text-center mb-12">
+              Please enter the otp to proceed
+            </p>
+
+            <form onSubmit={handleOtpVerification} className="space-y-4">
+              <div className="mb-2">
+                <input
+                  type="number"
+                  value={otp}
+                  onChange={(e) => setOtp(e.target.value)}
+                  placeholder="OTP"
+                  className="w-full px-4 py-3 border border-black rounded-md focus:outline-none "
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="border-2 mt-5 border-black w-full font-semibold bg-black text-white py-2 rounded-lg hover:bg-white hover:text-black transition"
+              >
+                VERIFY OTP
+              </button>
+            </form>
+          </div>
+        </div>
+
+        {/* RIGHT SIDE */}
+
+        <div className="hidden w-full md:w-1/2 bg-black text-white md:flex flex-col items-center justify-center p-8 rounded-tl-[80px] rounded-bl-[80px] ">
+          <div className="text-center h-[400px]">
+            <div className="flex justify-center mb-12">
+              <img
+                src={logo_with_title}
+                alt="logo"
+                className="h-auto w-80 mb-12"
+              />
+            </div>
+
+            <p className="text-gray-300 mb-12">
+              New to our platform? Sign up now.
+            </p>
+
+            <Link
+              to={"/register"}
+              className="border-2 mt-5 border-white w-full font-semibold bg-black text-white px-8 py-2 rounded-lg hover:bg-white hover:text-black transition"
+            >
+              SIGN UP
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default OTP;
+
+
